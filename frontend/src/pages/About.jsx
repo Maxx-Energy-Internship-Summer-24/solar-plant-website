bcb7f3844e0ff752c74c990704fab0d12daa6a29
Ty Reid <tybreid24@gmail.com>
2024-08-27 12:38:10 -0400

    Added content to the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,10 +4,24 @@ import teamwork from '../images/teamwork.jpg'
 const About = () => {
   return (
     <div className="App">
-      <header className="App-header flex items-center justify-center min-h-screen bg-gray-100">
-        <h1>About Us</h1>
-        <p>This is a simple homepage built with React.</p>
-        <button onClick={() => alert('Button clicked!')}>Click Me</button>
+      <header className="App-header flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-6xl font-bold m-10">About Us</h1>
+        <div className="m-[10%]">
+          <p>At MAXX Energy, we are committed to advancing the future of sustainable energy through
+            innovative technology and data-driven solutions. Our latest initiative aims to enhance
+            how we and our stakeholders access and analyze solar plant generation data.</p>
+          <p>Recognizing the need for an efficient, user-friendly platform, our executive
+            team has embarked on developing a web application designed to securely provide
+            real-time data insights to authorized users while maintaining transparency with publicly
+            viewable information. This application will empower our employees and stakeholders by
+            offering them an intuitive interface to explore and understand our solar plant data.</p>
+        </div>
+        <button 
+          onClick={() => alert('Button clicked!')}
+          className='px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50'
+          >
+            Click Me
+        </button>
       </header>
       <div>
         <img 
@@ -17,7 +31,14 @@ const About = () => {
           width: '100vw'
         }}
          />
-        hello
+      </div>
+      <div className="App-header flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-6xl font-bold m-[10%]">Meet the Team</h1>
+        <div className="grid grid-cols-3 justify-items-center gap-4 w-[100%]">
+          <h1 className='text-4xl font-bold'>DevOps</h1>
+          <h1 className='text-4xl font-bold'>Support</h1>
+          <h1 className='text-4xl font-bold'>Data</h1>
+        </div>
       </div>
     </div>
   )
