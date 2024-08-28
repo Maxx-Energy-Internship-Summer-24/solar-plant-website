107022a01099785cbd2b68e49398b0780ef42b4f
alfredchukwuma <dumebichukwuma@gmail.com>
2024-08-28 12:10:05 -0400

    Fixed another typo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,10 +13,10 @@ const Nabber = () => {
   return (
     <nav style={nabberStyle}>
       <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+      <Link to="/About" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
       <Link to="/FAQ" style={{ color: 'white', textDecoration: 'none' }}>FAQ</Link>
-      <Link to="/data" style={{ color: 'white', textDecoration: 'none' }}>Data</Link>
-      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }}>Login</Link>
+      <Link to="/Data" style={{ color: 'white', textDecoration: 'none' }}>Data</Link>
+      <Link to="/Login" style={{ color: 'white', textDecoration: 'none' }}>Login</Link>
     </nav>
   );
 };
