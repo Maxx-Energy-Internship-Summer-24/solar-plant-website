502c62ef2cc6e896fe36c028cbb3654ac3b47880
itsjahaziel <jahazielr116@gmail.com>
2024-08-28 11:38:21 -0400

    Hola

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import FAQ from './pages/FAQ'
-import Data from './pages/Data'
-import Login from './pages/Login'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import FAQ from "./pages/FAQ";
+import Data from "./pages/Data";
+import Login from "./pages/Login";
 
-import './styles/global.css'
-
+import "./styles/global.css";
 
 const App = () => {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
   useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-      }
-    )
-  }, [])
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-          />
-        <Route
-          path='/about'
-          element={<About />}
-          />
-        <Route
-          path='/FAQ'
-          element={<FAQ />}
-          />
-        <Route
-          path='/data'
-          element={<Data />}
-          />
-        <Route
-          path='/login'
-          element={<Login />}
-          />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/data" element={<Data />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
