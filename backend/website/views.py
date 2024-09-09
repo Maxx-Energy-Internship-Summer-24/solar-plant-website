2413320fb9ce110fffa43589a4a832a737b658c1
Ty Reid <tybreid24@gmail.com>
2024-09-09 12:23:00 -0400

    Start to add backend functionality.

--- backend/website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, Flask
-
-views = Blueprint('views', __name__)
-app = Flask(__name__)
-
-@views.route('/members')
-def home():
-    return {"members": ["Member1", "Member2", "Member3"]}
